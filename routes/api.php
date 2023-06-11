acfa7d34e83b43aad137025697988daf216f85d9
leandro-de-paula <leandrodepaula.ti@gmailcom>
2023-06-11 12:46:36 -0300

    Step 1

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/teste', function(){
+    return "teste com sucesso";
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
