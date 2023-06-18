754aaca6718ae5ab1f2b107b406ff2d94bbeb900
leandro-de-paula <leandrodepaula.ti@gmailcom>
2023-06-18 12:17:53 -0300

    Step 3

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestamentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/teste', function(){
     return "teste com sucesso";
 });
+
+Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
+Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
