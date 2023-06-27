0bc8ba20e8518112e812fad121f89f5d7b34a208
leandro-de-paula <leandrodepaula.ti@gmailcom>
2023-06-27 07:00:14 -0300

    Step 5 - Update Route

--- routes/api.php
+++ routes/api.php
@@ -21,23 +21,37 @@ Route::get('/teste', function(){
     return "teste com sucesso";
 });
 
-Route::get('/testamento', [TestamentoController::class, 'index']);
-Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
-Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
-Route::post('/testamento', [TestamentoController::class, 'store']);
-Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+// 1 -> Primeira forma de rota
+// Route::get('/testamento', [TestamentoController::class, 'index']);
+// Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+// Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
+// Route::post('/testamento', [TestamentoController::class, 'store']);
+// Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
 
-Route::get('/livro', [LivroController::class, 'index']);
-Route::get('/livro/{id}', [LivroController::class, 'show']);
-Route::put('/livro/{id}', [LivroController::class, 'update']);
-Route::post('/livro', [LivroController::class, 'store']);
-Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
+// Route::get('/livro', [LivroController::class, 'index']);
+// Route::get('/livro/{id}', [LivroController::class, 'show']);
+// Route::put('/livro/{id}', [LivroController::class, 'update']);
+// Route::post('/livro', [LivroController::class, 'store']);
+// Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
 
-Route::get('/versiculo', [VersiculoController::class, 'index']);
-Route::get('/versiculo/{id}', [VersiculoController::class, 'show']);
-Route::put('/versiculo/{id}', [VersiculoController::class, 'update']);
-Route::post('/versiculo', [VersiculoController::class, 'store']);
-Route::delete('/versiculo/{id}', [VersiculoController::class, 'destroy']);
+// Route::get('/versiculo', [VersiculoController::class, 'index']);
+// Route::get('/versiculo/{id}', [VersiculoController::class, 'show']);
+// Route::put('/versiculo/{id}', [VersiculoController::class, 'update']);
+// Route::post('/versiculo', [VersiculoController::class, 'store']);
+// Route::delete('/versiculo/{id}', [VersiculoController::class, 'destroy']);
+
+// 2 -> Segunda forma de rota
+// Route::apiResource('versiculo',VersiculoController::class);
+// Route::apiResource('livro',LivroController::class);
+// Route::apiResource('testamento',TestamentoController::class);
+
+
+// 3 -> Terceira forma de rota
+Route::apiResources([
+    'versiculo' => VersiculoController::class,
+    'livro' => LivroController::class,
+    'testamento' => TestamentoController::class,
+]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
